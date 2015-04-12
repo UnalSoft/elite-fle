6696c8125f4d6e841d6af9d1dd202b8f5ecdbc70
Jacques KAISER <jackokaiser@gmail.com>
2015-04-12 19:38:46 +0200

    fixed the 'ga .' to '%' regex typo and slightly modified order of appliance

--- src/main/webapp/resources/js/typographyFix.js
+++ src/main/webapp/resources/js/typographyFix.js
@@ -11,16 +11,16 @@
 
   // format all text node according to french typography
   _.forEach(textNodes, function(node) {
-    node.data = node.data.replace(/\s*\'\s*/g, "\'") // no space around apostrophe
+    node.data = node.data.replace(/\s*\;\s*/g, " \; ") // space around semi-colon
+      .replace(/\s*\:\s*/g, " \: ") // space around colon
+      .replace(/\s*\?\s*/g, " \? ") // space around interrogation
+      .replace(/\s*\!\s*/g, " \! ") // space around exclamation
       .replace(/\s*\(\s*/g, " \(") // left space left parentheses
       .replace(/\s*\)\s*/g, "\) ") // right space right parentheses
       .replace(/\s*\,\s*/g, "\, ") // space after coma
       .replace(/\s*\-\s*/g, "\-") // no space around dash
-      .replace(/\s*\;\s*/g, " \; ") // space around semi-colon
-      .replace(/\s*\:\s*/g, " \: ") // space around colon
-      .replace(/\s*\?\s*/g, " \? ") // space around interrogation
-      .replace(/\s*\!\s*/g, " \! ") // space around exclamation
-      .replace(/\s*\!\s*/g, " \% ") // space around percentage
+      .replace(/\s*\%\s*/g, " \% ") // space around percentage
+      .replace(/\s*\'\s*/g, "\'") // no space around apostrophe
       .replace(/\s*\.\s*/g, "\. "); // space after point
 
   });
