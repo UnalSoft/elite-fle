95406f075ae036f20e7e913f8efda52bcf67a02d
Jacques KAISER <jackokaiser@gmail.com>
2015-04-12 15:17:53 +0200

    fixed typography rules for colon and semi-colon, added scripts to systemization activities too

--- src/main/webapp/resources/js/typographyFix.js
+++ src/main/webapp/resources/js/typographyFix.js
@@ -7,19 +7,21 @@
     });
   };
 
-  var textNodes = getTextNodesIn(document.getElementById("spottingActivityForm:spottingActivityTextPanel_content"));
+  var textNodes = getTextNodesIn(document.getElementById("spottingActivityForm"));
 
   // format all text node according to french typography
   _.forEach(textNodes, function(node) {
     node.data = node.data.replace(/\s*\'\s*/g, "\'") // no space around apostrophe
       .replace(/\s*\(\s*/g, " \(") // left space left parentheses
       .replace(/\s*\)\s*/g, "\) ") // right space right parentheses
-      .replace(/\s*\.\s*/g, "\. ") // space after point
       .replace(/\s*\,\s*/g, "\, ") // space after coma
       .replace(/\s*\-\s*/g, "\-") // no space around dash
-      .replace(/\s*\;\s*/g, "\; ") // space after semi-colon
-      .replace(/\s*\:\s*/g, "\: ") // space after colon
+      .replace(/\s*\;\s*/g, " \; ") // space around semi-colon
+      .replace(/\s*\:\s*/g, " \: ") // space around colon
       .replace(/\s*\?\s*/g, " \? ") // space around interrogation
-      .replace(/\s*\!\s*/g, " \! "); // space around exclamation
+      .replace(/\s*\!\s*/g, " \! ") // space around exclamation
+      .replace(/\s*\!\s*/g, " \% ") // space around percentage
+      .replace(/\s*\.\s*/g, "\. "); // space after point
+
   });
 })();
